228dc755bec075b1718d50a1c2ee583789b12bcf
Alexclownfish <63449830+alexclownfish@users.noreply.github.com>
2022-05-10 16:19:31 +0800

    增加全量备份数据滚动删除脚本

---
+++ shell/mysql增量全量备份/whole_clean.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ReservedNum=6  #保留文件数
+FileDir=/opt/sh/mysql.bak.test/test_whole_clear/cleandir
+date=$(date "+%Y%m%d-%H%M%S")
+
+cd $FileDir   #进入备份目录
+FileNum=$(ls -l | grep '^d' | wc -l)   #当前有几个文件夹，即几个备份
+
+while(( $FileNum > $ReservedNum))
+do
+    OldFile=$(ls -rt | head -1)         #获取最旧的那个备份文件夹
+    echo  $date "Delete File:"$OldFile
+    rm -rf $FileDir/$OldFile
+    let "FileNum--"
+done
