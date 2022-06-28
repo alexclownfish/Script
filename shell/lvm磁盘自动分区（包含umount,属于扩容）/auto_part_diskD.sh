f685b1f50cdb039ffecd1fc4fa98e5b74b2e858c
Alexclownfish <63449830+alexclownfish@users.noreply.github.com>
2022-06-28 09:00:38 +0800

    Create auto_part_diskD.sh

---
+++ shell/lvm磁盘自动分区（包含umount,属于扩容）/auto_part_diskD.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#author: ywz
+echo -e "\e[1;42mGet Disk Params!!!!!\e[0m"
+Disk=/dev/vda
+#Get the number of existing disks
+Total=`fdisk -l | grep "/dev/vda" | awk {'print $1'} | wc -l`
+NeedCount=`expr ${Total} - 1`
+DiskNum=`expr ${NeedCount} + 1`
+DiskName="$Disk$DiskNum"
+echo -e "\e[1;42mStep2: Parting the disks....\e[0m"
+echo "
+     n
+     p
+      
+      
+      
+     t
+      
+     8e
+     w" | fdisk $Disk &> /dev/null
+partprobe &> /dev/null
+echo -e "\e[1;32mPart finished!!!!\e[0m"
+sleep 3
+echo -e "\e[1;42mStep3: Formating disks....\e[0m"
+pvcreate $DiskName &> /dev/null
+vgextend centos $DiskName &> /dev/null
+lvextend /dev/centos/root $DiskName &> /dev/null
+xfs_growfs /dev/centos/root &> /dev/null
+echo -e "\e[1;32mFormat finished!!!!\e[0m"
