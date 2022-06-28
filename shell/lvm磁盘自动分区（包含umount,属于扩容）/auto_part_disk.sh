b6e457ae904233c2e2bf2ee458986e6ea3749ac8
Alexclownfish <63449830+alexclownfish@users.noreply.github.com>
2022-06-28 08:59:24 +0800

    Update auto_part_disk.sh

--- shell/lvm磁盘自动分区（包含umount,属于扩容）/auto_part_disk.sh
+++ shell/lvm磁盘自动分区（包含umount,属于扩容）/auto_part_disk.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
-#author: https://blogtest.alexcld.com https://vue.alexcld.com https://blog.csdn.net/weixin_45509582
+#author: ywz
 #"---------"包括这部分是unmount操作可以注释掉，如果不需要交互则将disk取消注释，Step3以上全部注释即可
-#disk=/dev/vda
+Disk=/dev/vda
+Total=`fdisk -l | grep "/dev/vda" | awk {'print $1'} | wc -l`
+NeedCount=`expr ${Total} - 1`
+DiskNum=`expr ${NeedCount} + 1`
+DiskName="$Disk$DiskNum"
 echo -e "\e[1;42mDisk partion scripts!!!!!\e[0m"
 echo -e "\e[1;32mDisk list:\e[0m"
 fdisk -l | grep -o "Disk /dev/[a-z]d[a-z]"
 read -p "choose disk:" disk
-if [ $disk == q ]; then
+if [ $Disk == q ]; then
 	echo "quiting..."
 	sleep 1
 	echo "quited!!"
 	exit 1
 fi
 #------------------------------------unmount已有磁盘（根据需求选择）----------------------------------
-until fdisk -l | grep -o "Disk /dev/[a-z]d[a-z]" | grep "^Disk $disk$" &> /dev/null;do
+until fdisk -l | grep -o "Disk /dev/[a-z]d[a-z]" | grep "^Disk $Disk$" &> /dev/null;do
 	fdisk -l | grep -o "Disk /dev/[a-z]d[a-z]"
 	read -p "unknown option,choose again:" disk
 done
@@ -32,14 +36,14 @@ if [ $des == "n" ];then
 fi
 
 echo -e "\e[1;42mStep1: Checking the umounting th mounted disks....\e[0m"
-for I in `mount | grep "$disk" | awk -F " " '{print $1}'`;do
+for I in `mount | grep "$Disk" | awk -F " " '{print $1}'`;do
 	fuser -km  $I
 	umount $I
 done
 echo -e "\e[1;32mUnmount finished!!!\e[0m"
 sleep 1
 echo -e "\e[1;42mStep2: Initialing the disks....\e[0m"
-dd if=/dev/zero of=$disk bs=512 count=1
+dd if=/dev/zero of=$Disk bs=512 count=1
 sync
 sleep 3
 echo -e "\e[1;32mInitial finished!!!!\e[0m"
@@ -55,13 +59,13 @@ echo "
      t
       
      8e
-     w" | fdisk $disk &> /dev/null
+     w" | fdisk $Disk &> /dev/null
 partprobe &> /dev/null
 echo -e "\e[1;32mPart finished!!!!\e[0m"
 sleep 3
 echo -e "\e[1;42mStep4: Formating disks....\e[0m"
-pvcreate ${disk}3 &> /dev/null
-vgextend centos ${disk}3 &> /dev/null
-lvextend /dev/centos/root ${disk}3 &> /dev/null
+pvcreate $DiskName &> /dev/null
+vgextend centos $DiskName &> /dev/null
+lvextend /dev/centos/root $DiskName &> /dev/null
 xfs_growfs /dev/centos/root &> /dev/null
 echo -e "\e[1;32mFormat finished!!!!\e[0m"
